c6ecebb051cff7494f23b145030ceadc98af5594
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2023-08-13 15:11:35 +0330

    add hash function

--- Block/block.test.js
+++ Block/block.test.js
@@ -21,4 +21,21 @@ describe("Block", () => {
             expect(genesisBlock).toEqual(GENESIS_DATA);
         }); 
     });
+    describe("mineBlock", ()=>{
+        const lastBlock = Block.genesis();
+        const data = "mined data";
+        const minedBlock =  Block.mineBlock({lastBlock,data}) 
+        it("return a bock instance", ()=>{
+            expect(minedBlock instanceof Block).toEqual(true);
+        });
+        it("sets the `lastHash` to the `hash` of the lastBlock",()=>{
+            expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+        });
+        it("sets the `data`",()=>{
+            expect(minedBlock.data).toEqual(data);
+        });
+        it("sets the `timestamp`",()=>{
+            expect(minedBlock.timestamp).not.toEqual(undefined);
+        });
+    });
 });
